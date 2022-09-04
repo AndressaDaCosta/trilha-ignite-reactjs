ff7ebfa189951247c4181928652e603d1fe8cc4e
Andressa Da Costa <andressadacostan@gmail.com>
2022-09-04 20:16:33 -0300

    comments

---
+++ 01-fundamentos-reactjs/src/example.jsx
@@ -0,0 +1,16 @@
+// props: { author: "", content: "" } ex: console.log(props)
+
+
+
+// export function Post(props) {
+
+//   return (
+//   <div>
+//     <strong>{props.author}</strong>
+//     <p>{props.content}</p>
+//     </div>
+//   )
+// }
+
+// Defaut Exports vs Named Exports
+// export default Post
