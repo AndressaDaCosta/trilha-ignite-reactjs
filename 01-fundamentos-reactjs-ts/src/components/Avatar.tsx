5927f1d1cbd3deef0d6d82e80d712d3014c440d2
Andressa Da Costa <andressadacostan@gmail.com>
2022-09-05 10:11:21 -0300

    Extensão de interfaces

--- 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
@@ -1,16 +1,25 @@
+import { ImgHTMLAttributes } from "react";
+
 import styles from './Avatar.module.css';
 
-interface AvatarProps {
+
+
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
 	hasBorder?: boolean;
-	src: string;
-	alt?: string;
   }
-  
-  export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+
+// interface AvatarProps {
+// 	hasBorder?: boolean;
+// 	src: string;
+// 	alt?: string;
+//   }
+											//rest operator   
+  export function Avatar({ hasBorder = true, ...props}: AvatarProps) {
 	return (
 		<img
 			className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-			src={src}
+			{...props}
+			// ... spread operator
 		/>
 	);
 }
